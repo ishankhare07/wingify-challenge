d8620046b900e5692b0a2145ea886930a04b31c0
Ishan Khare <ishankhare07@gmail.com>
2016-06-26 14:09:50 +0530

    rename to avoid confusion
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- app/rest/models/item.py
+++ app/rest/models/item.py
@@ -1,5 +1,6 @@
 from passlib.apps import custom_app_context as pwd_context
 from . import db
+import json
 
 class Item(db.Model):
     __tablename__ = 'items'
@@ -10,9 +11,16 @@ class Item(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    user = db.relationship("users", back_populates='items')
+    user = db.relationship("User", back_populates='items')
 
     def __repr__(self):
         return '<Item: %r, %r, %r>' %(self.id, self.name, self.quantity)
 
-
+    def make_json(self):
+        return json.dumps({
+            "item_id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "quantity": self.quantity,
+            "user_id": self.user_id
+            })
